c4402a67a1273560746436bcbfa61ed62a3a58b1
Todd Nguyen <todd.nguyen47@gmail.com>
2023-06-14 14:45:13 -0700

    Adding `time.After` in `chantrial_test`

--- pkg/chantrial/chantrial_test.go
+++ pkg/chantrial/chantrial_test.go
@@ -15,18 +15,20 @@ import (
 const _maxNumMessages = 100
 
 func Test_Chan(t *testing.T) {
+	// Reference: https://go.dev/blog/pipelines
 	c1 := make(chan string)
-	doneChannel := make(chan int)
+	publisherDone := make(chan struct{})
+	subscriberDone := make(chan struct{})
 	totalCount := uint64(0)
-	go subscriber(c1, doneChannel, &totalCount)
-	go publisher(c1)
+	go subscriber(c1, publisherDone, subscriberDone, &totalCount)
+	go publisher(c1, publisherDone)
 
-	<-doneChannel
+	<-subscriberDone
 	fmt.Printf("Total count: %d\n", totalCount)
 	assert.Equal(t, _maxNumMessages, int(totalCount))
 }
 
-func publisher(chanString chan string) {
+func publisher(chanString chan string, publisherDone chan struct{}) {
 
 	reader := rand.Reader
 	min := int64(10)
@@ -36,51 +38,55 @@ func publisher(chanString chan string) {
 		if i > 0 {
 			bigInt, _ := rand.Int(reader, max)
 			bigInt2 := bigInt.Int64() + min
-			sleepTime := time.Duration(bigInt2)
+			sleepTime := time.Duration(bigInt2) * time.Millisecond
 			fmt.Printf("Sleeping for: %s\n", sleepTime.String())
-			time.Sleep(sleepTime * time.Millisecond)
+			time.Sleep(sleepTime)
 		}
 		msg := fmt.Sprintf("Number %d", i)
 		//fmt.Println(msg)
 		chanString <- msg
 	}
-
-	// Close channel as we are the sender, otherwise channel will keep being opened
-	close(chanString)
+	// We are done now; send signal
+	publisherDone <- struct{}{}
 }
 
-func subscriber(chanString <-chan string, doneChannel chan int, totalCount *uint64) {
-	elems := make([]string, 0)
-	start := time.Now().UTC()
+func subscriber(chanString <-chan string, publisherDone <-chan struct{}, subscriberDone chan struct{},
+	totalCount *uint64) {
 
-	for elem := range chanString {
-		elems = append(elems, elem)
-		now := time.Now().UTC()
-		dur := now.Sub(start)
-		lenElems := len(elems)
-		if lenElems >= 5 || dur.Milliseconds() > 200 {
-			doWork(elems, dur, totalCount)
+	elems := make([]string, 0)
+
+	for {
+		select {
+		case elem := <-chanString:
+			// We received an element
+			elems = append(elems, elem)
+			lenElems := len(elems)
+			if lenElems >= 5 {
+				doWork(elems, totalCount)
+				// Reset now
+				elems = make([]string, 0)
+			}
+		case <-time.After(75 * time.Millisecond):
+			// Timed out
+			fmt.Println("timed out")
+			doWork(elems, totalCount)
 			// Reset now
 			elems = make([]string, 0)
-			start = time.Now().UTC()
+		case <-publisherDone:
+			// Do any work on the remaining elements
+			fmt.Println("last stretch!")
+			doWork(elems, totalCount)
+			// Reset now
+			elems = make([]string, 0)
+			// Notify that subscriber is finished
+			subscriberDone <- struct{}{}
 		}
 	}
-
-	// Any remaining elements
-	if len(elems) > 0 {
-		fmt.Println("Finishing remaining elements")
-		now := time.Now().UTC()
-		dur := now.Sub(start)
-		doWork(elems, dur, totalCount)
-	}
-
-	// Signals that the subscriber is done
-	doneChannel <- 1
 }
 
-func doWork(elems []string, dur time.Duration, totalCount *uint64) {
+func doWork(elems []string, totalCount *uint64) {
 	lenElems := len(elems)
-	fmt.Printf("There are currently %d items in the list. Time duration: %s \n", lenElems, dur.String())
+	fmt.Printf("There are currently %d items in the list. \n", lenElems)
 	msg := strings.Join(elems, " @@@ ")
 	fmt.Println("msg: " + msg)
 	atomic.AddUint64(totalCount, uint64(lenElems))
