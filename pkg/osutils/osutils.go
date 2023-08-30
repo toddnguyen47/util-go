9e17e7bdb7084acd0ea841a22f096d5ab794ccba
Todd Nguyen <todd.nguyen47@gmail.com>
2023-08-30 16:46:20 -0700

    Add more get env with default functions to `osutils`

--- pkg/osutils/osutils.go
+++ pkg/osutils/osutils.go
@@ -2,6 +2,7 @@ package osutils
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +14,33 @@ func GetEnvWithDefault(key string, defaultStr string) string {
 		val = defaultStr
 	}
 	return val
+}
+
+func GetEnvWithDefaultInt(key string, defaultInt int) int {
+	val := strings.TrimSpace(os.Getenv(key))
+	valInt, err := strconv.Atoi(val)
+	if err != nil {
+		return defaultInt
+	}
+	return valInt
+}
+
+func GetEnvWithDefaultFloat64(key string, defaultFloat float64) float64 {
+	val := strings.TrimSpace(os.Getenv(key))
+	valInt, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return defaultFloat
+	}
+	return valInt
+}
+
+func GetEnvWithDefaultBool(key string, defaultBool bool) bool {
+	val := strings.TrimSpace(os.Getenv(key))
+	valBool, err := strconv.ParseBool(val)
+	if err != nil {
+		return defaultBool
+	}
+	return valBool
 }
 
 func RemoveIfExists(path string) error {
--- pkg/osutils/osutils_test.go
+++ pkg/osutils/osutils_test.go
@@ -1,44 +1,121 @@
 package osutils
 
 import (
+	"context"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-const testKey = "testKey"
-const testValue = "testValue"
-const defaultValue = "defaultValue"
+const (
+	testKey      = "testKey"
+	testValue    = "testValue"
+	defaultValue = "defaultValue"
+)
 
-func SetUp(t *testing.T) {
+// ############################################################################
+// #region SETUP
+// ############################################################################
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing context
+type OsUtilsTestSuite struct {
+	suite.Suite
+	ctxBg context.Context
+}
+
+func (s *OsUtilsTestSuite) SetupTest() {
+	s.resetMonkeyPatching()
+	s.ctxBg = context.Background()
 	err := os.Setenv(testKey, "")
-	assert.Nil(t, err)
+	assert.Nil(s.T(), err)
 }
 
-func Test_GivenEnvVarExists_ThenReturnVal(t *testing.T) {
-	SetUp(t)
+func (s *OsUtilsTestSuite) TearDownTest() {
+	s.resetMonkeyPatching()
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestOsUtilsTestSuite(t *testing.T) {
+	suite.Run(t, new(OsUtilsTestSuite))
+}
+
+// #endregion
+
+// ############################################################################
+// #region TESTS ARE BELOW
+// ############################################################################
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarExists_ThenReturnVal() {
 	err := os.Setenv(testKey, testValue)
-	assert.Nil(t, err)
+	assert.Nil(s.T(), err)
 	val := GetEnvWithDefault(testKey, defaultValue)
-	assert.Equal(t, testValue, val)
+	assert.Equal(s.T(), testValue, val)
 }
 
-func Test_GivenEnvVarDoesNotExist_ThenReturnDefault(t *testing.T) {
-	SetUp(t)
+func (s *OsUtilsTestSuite) Test_GivenEnvVarDoesNotExist_ThenReturnDefault() {
 	val := GetEnvWithDefault(testKey, defaultValue)
-	assert.Equal(t, defaultValue, val)
+	assert.Equal(s.T(), defaultValue, val)
 }
 
-func Test_GivenNonExistentFile_ThenDoNotRemove(t *testing.T) {
+func (s *OsUtilsTestSuite) Test_GivenNonExistentFile_ThenDoNotRemove() {
 	err := RemoveIfExists("asdf")
-	assert.Nil(t, err)
+	assert.Nil(s.T(), err)
 }
 
-func Test_GivenExistentFile_ThenRemove(t *testing.T) {
+func (s *OsUtilsTestSuite) Test_GivenEnvVarExists_WhenInt_ThenReturnVal() {
+	err := os.Setenv(testKey, "5")
+	assert.Nil(s.T(), err)
+	val := GetEnvWithDefaultInt(testKey, 100)
+	assert.Equal(s.T(), 5, val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarDoesNotExists_WhenInt_ThenReturnDefaultValue() {
+	val := GetEnvWithDefaultInt(testKey, 100)
+	assert.Equal(s.T(), 100, val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarExists_WhenFloat64_ThenReturnVal() {
+	err := os.Setenv(testKey, "5.5")
+	assert.Nil(s.T(), err)
+	val := GetEnvWithDefaultFloat64(testKey, 42.2)
+	assert.Equal(s.T(), 5.5, val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarDoesNotExists_WhenFloat64_ThenReturnDefaultValue() {
+	val := GetEnvWithDefaultFloat64(testKey, 42.2)
+	assert.Equal(s.T(), 42.2, val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarExists_WhenBool_ThenReturnVal() {
+	err := os.Setenv(testKey, "true")
+	assert.Nil(s.T(), err)
+	val := GetEnvWithDefaultBool(testKey, false)
+	assert.True(s.T(), val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenEnvVarDoesNotExists_WhenBool_ThenReturnDefaultValue() {
+	val := GetEnvWithDefaultBool(testKey, false)
+	assert.False(s.T(), val)
+}
+
+func (s *OsUtilsTestSuite) Test_GivenExistentFile_ThenRemove() {
 	filePath := "helloWorld.txt"
 	err := os.WriteFile(filePath, []byte("hello world"), 0666)
-	assert.Nil(t, err)
+	assert.Nil(s.T(), err)
 	err = RemoveIfExists(filePath)
-	assert.Nil(t, err)
+	assert.Nil(s.T(), err)
 }
+
+// ############################################################################
+// #region TEST HELPERS
+// ############################################################################
+
+func (s *OsUtilsTestSuite) resetMonkeyPatching() {
+}
+
+// #endregion
