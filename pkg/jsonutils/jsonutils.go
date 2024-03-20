b95dc854e181b25856f4ff553ada8b7a96b03ed3
Todd Nguyen <todd.nguyen47@gmail.com>
2024-03-20 16:12:47 -0700

    Add a `TrimRight()` in `MarshalNoEscapeHtml()`

--- pkg/jsonutils/jsonutils.go
+++ pkg/jsonutils/jsonutils.go
@@ -3,9 +3,11 @@ package jsonutils
 import (
 	"bytes"
 	"encoding/json"
-	"io"
 )
 
+// MarshalNoEscapeHtml - marshal JSON without escaping HTML
+// Ref: https://stackoverflow.com/a/28596225/6323360
+// Ref for trimming `\n`: https://stackoverflow.com/questions/28595664/how-to-stop-json-marshal-from-escaping-and#comment122847570_28596225
 func MarshalNoEscapeHtml(v any) ([]byte, error) {
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
@@ -14,5 +16,6 @@ func MarshalNoEscapeHtml(v any) ([]byte, error) {
 	if err != nil {
 		return make([]byte, 0), err
 	}
-	return io.ReadAll(&buf)
+	b1 := bytes.TrimRight(buf.Bytes(), "\n")
+	return b1, nil
 }
